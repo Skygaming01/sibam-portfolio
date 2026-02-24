3ac526cc99431ac679dc87620f926d73bfc07deb
Skygaming01 <157948759+Skygaming01@users.noreply.github.com>
2026-02-24 11:50:34 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Lightning flash effect every 8 seconds
-setInterval(() => {
-  document.body.style.backgroundColor = "#1a1a1a";
-  setTimeout(() => {
-    document.body.style.backgroundColor = "#0f0f0f";
-  }, 200);
-}, 8000);
